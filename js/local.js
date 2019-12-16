427eb8ab0e46cb2405aa9e2b755cb79b06084d58
GlennOnyango <glennonyango@yahoo.com>
2019-12-16 11:21:54 +0300

    This is a redo of the join us

--- js/local.js
+++ js/local.js
@@ -1,28 +1,28 @@
 $('document').ready(function() {
+    /*
+        $('#open_mod').click(function() {
+            $('#sin_form').removeClass("d-none");
+            $('#sup_form').addClass("d-none");
+            $('#sin').addClass("d-none");
+            $('#sup').removeClass("d-none");
+            $('#exampleModalLabel').text("Login to LeafBerri");
+        });
+        $('#sup').click(function() {
+            $('#sin_form').addClass("d-none");
+            $('#sup_form').removeClass("d-none");
+            $('#sin').removeClass("d-none");
+            $('#sup').addClass("d-none");
+            $('#exampleModalLabel').text("SignUp with LeafBerri");
 
-    $('#open_mod').click(function() {
-        $('#sin_form').removeClass("d-none");
-        $('#sup_form').addClass("d-none");
-        $('#sin').addClass("d-none");
-        $('#sup').removeClass("d-none");
-        $('#exampleModalLabel').text("Login to LeafBerri");
-    });
-    $('#sup').click(function() {
-        $('#sin_form').addClass("d-none");
-        $('#sup_form').removeClass("d-none");
-        $('#sin').removeClass("d-none");
-        $('#sup').addClass("d-none");
-        $('#exampleModalLabel').text("SignUp with LeafBerri");
+        });
+        $('#sin').click(function() {
+            $('#sin_form').removeClass("d-none");
+            $('#sup_form').addClass("d-none");
+            $('#sin').addClass("d-none");
+            $('#sup').removeClass("d-none");
+            $('#exampleModalLabel').text("Login to LeafBerri");
 
-    });
-    $('#sin').click(function() {
-        $('#sin_form').removeClass("d-none");
-        $('#sup_form').addClass("d-none");
-        $('#sin').addClass("d-none");
-        $('#sup').removeClass("d-none");
-        $('#exampleModalLabel').text("Login to LeafBerri");
-
-    });
-
+        });
+    */
 
 });
